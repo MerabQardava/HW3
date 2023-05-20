1f07c61162a234e1abc3d3729db66630bc49356e
MerabQardava <merab.kardava.1@btu.edu.ge>
2023-05-20 21:29:56 +0400

    hw

--- app/src/main/java/com/example/firebaseappbtu1/ProfileActivity.kt
+++ app/src/main/java/com/example/firebaseappbtu1/ProfileActivity.kt
@@ -18,8 +18,12 @@ class ProfileActivity : AppCompatActivity() {
 
     private lateinit var linkEditText : EditText
     private lateinit var usernameEditText : EditText
+    private lateinit var surnameEditText : EditText
+    private lateinit var PnEditText : EditText
     private lateinit var editProfileButton : Button
     private lateinit var userNameTextView : TextView
+    private lateinit var pnTextView : TextView
+    private lateinit var emailTextView : TextView
     private lateinit var imageView : ImageView
     val auth = FirebaseAuth.getInstance()
     val db = FirebaseDatabase.getInstance().getReference("User")
@@ -33,10 +37,12 @@ class ProfileActivity : AppCompatActivity() {
     private fun listeners() {
         editProfileButton.setOnClickListener {
             val link = linkEditText.text.toString()
-            val username = usernameEditText.text.toString()
-            val userInfo = User(auth.currentUser?.email,
+            val name = usernameEditText.text.toString()
+            val surname = surnameEditText.text.toString()
+            val personalNum = PnEditText.text.toString()
+            val userInfo = User(name,surname,auth.currentUser?.email,
                 auth.currentUser!!.uid,
-                link, username)
+                link,personalNum)
             db.child(auth.currentUser!!.uid).setValue(userInfo)
         }
     }
@@ -44,8 +50,12 @@ class ProfileActivity : AppCompatActivity() {
     private fun init(){
         linkEditText = findViewById(R.id.linkEditText)
         usernameEditText = findViewById(R.id.usernameEditText)
+        surnameEditText = findViewById(R.id.surname)
+        PnEditText = findViewById(R.id.PN)
         editProfileButton = findViewById(R.id.editProfileButton)
         userNameTextView = findViewById(R.id.userNameTextView)
+        pnTextView = findViewById(R.id.PnTextView)
+        emailTextView = findViewById(R.id.emailTextView)
         imageView = findViewById(R.id.imageView)
 
         userNameTextView.text = auth.uid
@@ -53,9 +63,15 @@ class ProfileActivity : AppCompatActivity() {
         db.child(auth.uid!!).addValueEventListener(object : ValueEventListener{
             override fun onDataChange(snapshot: DataSnapshot) {
                 val userInfo = snapshot.getValue(User::class.java) ?: return
-                userNameTextView.text = userInfo.userName
-                val link = userInfo.link
-                Glide.with(this@ProfileActivity).load(link).into(imageView)
+                if(((userInfo.pn)?.length!=13)||((userInfo.pn)?.toDoubleOrNull()==null)){
+                    Toast.makeText(this@ProfileActivity, "piradi nomeri sheaswore\uD83D\uDE2D", Toast.LENGTH_SHORT).show()
+                }else {
+                    userNameTextView.text = userInfo.name + " " + userInfo.surname
+                    pnTextView.text = userInfo.pn
+                    emailTextView.text = userInfo.email
+                    val link = userInfo.link
+                    Glide.with(this@ProfileActivity).load(link).into(imageView)
+                }
             }
 
             override fun onCancelled(error: DatabaseError) {
--- app/src/main/java/com/example/firebaseappbtu1/User.kt
+++ app/src/main/java/com/example/firebaseappbtu1/User.kt
@@ -1,8 +1,15 @@
 package com.example.firebaseappbtu1
 
 data class User(
+    val name : String ?= null,
+    val surname: String ?=null,
     val email : String ?= null,
     val uid : String ?= null,
     val link : String ?= null,
-    val userName : String ?= null
+    val pn : String ?= null,
+
+)
+
+data class School(
+    val students: List<User>
 )
